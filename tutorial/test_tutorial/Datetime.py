b8dc666338c4516fe01cc8e0266b74711c1add77
Samuel Papp <samuel.papp@intranet.bearzi.it>
2026-06-03 14:54:11 +0200

    cancella

--- tutorial/test_tutorial/Datetime.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from zoneinfo import ZoneInfo  # Modulo integrato
-
-# Data e ora attuale in un fuso orario specifico
-roma_time = datetime.now(ZoneInfo("Europe/Rome"))
-
-day = roma_time.strftime("%A")
-hour = roma_time.strftime("%H")
-mint = roma_time.strftime("%M")
